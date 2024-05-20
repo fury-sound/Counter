3432467b46fcd44eda2e2d462bb6437bc8b69f9c
Valery Zvonarev <furysound1@yandex.ru>
2024-05-21 00:35:09 +0300

    Простая задача выполнена

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,8 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counter = 0
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var counterButton: UIButton!
+    
+    @IBAction func counterUp(_ sender: Any) {
+//        print("Pressed!")
+        counter += 1
+        counterLabel.text = "Значение счетчика: \n \(counter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = "Значение счетчика: \n \(counter)"
         // Do any additional setup after loading the view.
     }
 
