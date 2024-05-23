d7eb5a35755c93d4354be185229b6790eb709f84
Valery Zvonarev <furysound1@yandex.ru>
2024-05-23 16:25:23 +0300

    Усложненная задача выполнена

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,23 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var counter = 0
-    @IBOutlet weak var counterLabel: UILabel!
+    private var mainCounter = 0
+    private var lineCounter = 0
+    private let formatter = DateFormatter()
     
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet private weak var historyTextView: UITextView!
     
-    @IBAction func counterUp(_ sender: Any) {
-//        print("Pressed!")
-        counter += 1
-        counterLabel.text = "Значение счетчика: \n \(counter)"
+    @IBOutlet private weak var counterLabel: UILabel!
+    
+    @IBOutlet private weak var counterButton: UIButton!
+    
+    @IBOutlet private weak var counterButtonDown: UIButton!
+    
+    @IBAction private func counterButtonUp(_ sender: Any) {
+        lineCounter += 1
+        mainCounter += 1
+        counterLabel.text = "Значение счетчика: \n \(mainCounter)"
+        addingTextToTextView("\(lineCounter).\(getMyDate()): значение изменено на +1")
+    }
+    
+    @IBAction private func counterDown(_ sender: Any) {
+        lineCounter += 1
+        if mainCounter > 0 {
+            mainCounter -= 1
+            addingTextToTextView("\(lineCounter).\(getMyDate()): значение изменено на -1")
+        } else {
+            addingTextToTextView("\(lineCounter).\(getMyDate()): попытка уменьшить значение счётчика ниже 0")
+//            addingTextToTextView("\(getMyDate()): нельзя ниже 0")
+        }
+        counterLabel.text = "Значение счетчика: \n \(mainCounter)"
+    }
+    
+    @IBAction private func counterReset(_ sender: Any) {
+        lineCounter += 1
+        mainCounter = 0
+        counterLabel.text = "Значение счетчика: \n \(mainCounter)"
+//        historyTextView.text = "История изменений\nСчетчик сброшен"
+        addingTextToTextView("\(lineCounter).\(getMyDate()): значение сброшено")
+    }
+    
+    private func addingTextToTextView(_ textString: String) {
+        if let existingText = historyTextView.text {
+            historyTextView.text = "\(existingText)\n\(textString)"
+        } else {
+            historyTextView.text = textString
+        }
+        
+//        let range = NSMakeRange(historyTextView.text.count - 1, 0)
+//        historyTextView.scrollRangeToVisible(range)
+        let bottom = NSRange(location: historyTextView.text.count - 1, length: 1)
+        historyTextView.scrollRangeToVisible(bottom)
+    }
+    
+    private func getMyDate() -> String {
+        let currentDate = Date()
+        let myDate = formatter.string(from: currentDate)
+        return myDate
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counterLabel.text = "Значение счетчика: \n \(counter)"
-        // Do any additional setup after loading the view.
+        formatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+        counterLabel.text = "Значение счетчика: \n \(mainCounter)"
+        historyTextView.text = "История изменений\n"
+        historyTextView.isScrollEnabled = true
     }
-
-
 }
 
